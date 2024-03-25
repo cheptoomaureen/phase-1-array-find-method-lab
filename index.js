06f330fbe38a60fa590a38cb1fcb76c3cc9c82a6
Maureen Cheptoo <cheptoomaureen31@gmail.com>
2024-03-25 22:54:05 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record) {
+    // Use Array.prototype.find() to find the first entry with result 'W'
+    const winningEntry = record.find(entry => entry.result === 'W');
+  
+    return winningEntry ? winningEntry.year : undefined;
+  }
+  
